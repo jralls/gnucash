e4bdad80e354cbd5883bf7adf5910d4daf3dc50c
John Ralls <jralls@ceridwen.us>
2024-05-04 17:57:01 -0700

    Adjust CI files for gtk3to4 branch.
    Disable mac tests, it's not ready to build on macOS. Disable Ubuntu tests,
    the github ubuntu-latest runner provides Gtk-4.6 which isn't new enough.

--- util/ci/actions/archlinux-test/entrypoint.sh
+++ util/ci/actions/archlinux-test/entrypoint.sh
@@ -8,7 +8,7 @@ export TZ="America/Los_Angeles"
 export PATH="$PATH:/usr/bin/core_perl"
 export CTEST_OUTPUT_ON_FAILURE=On
 git config --global --add safe.directory /github/workspace
-cmake /github/workspace -DWITH_PYTHON=ON -DCMAKE_BUILD_TYPE=debug -G Ninja
+cmake /github/workspace -DWITH_PYTHON=ON -DWITH_AQBANKING=OFF -DCMAKE_BUILD_TYPE=debug -G Ninja
 ninja
 ninja check
 
